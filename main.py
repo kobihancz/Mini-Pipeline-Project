8a58c89eda42c76c556cf2c41e7752145619f65f
Kobi Hancz <kobihancz@Kobis-MacBook-Pro-2.local>
2021-05-10 11:49:49 -0700

    second commit

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 import mysql.connector
 
-def get_db_connection():
-    connection = None
-    try:
-        connection = mysql.connector.connect(user = 'root', 
-                                    password = 'Riley123$',
-                                    host = '127.0.0.1',
-                                    port = '3306',
-                                    database = 'PipelineMiniProject')
-    except Exception as error:
-        print("Error while connecting to database for job tracker", error)
+class Connect_toD:
+    def __init__(self,user,password,host,port,database):
+        try:
+            self.connection = mysql.connector.connect(user = 'root', 
+                                        password = 'Riley123$',
+                                        host = '127.0.0.1',
+                                        port = '3306',
+                                        database = 'PipelineMiniProject')
+        except Exception as ex:
+            print(f"Error while connecting to {database} as {user}", ex)
+        print(f"Sucssesfully connected to {database} as {user}")
 
-    return connection
+    def load_third_party(self,connection, file_path_csv):
+        insert_stmt = ("INSERT INTO sales" 
+                    "(ticket_id, trans_date, event_id, event_name, event_date, event_type, event_city, customer_id, price, num_tickets)" 
+                    "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+        with self.connection.cursor() as cursor:
+            with open(file_path_csv) as csv_file:
+                try:
+                    for row in csv.reader(csv_file):
+                        cursor.execute(insert_stmt,row)
+                    connection.commit()
+                    cursor.close()
+                    return
+                
+                except Exception as ex:
+                    print("Error while reading Sales CSV into PipelineMiniProject", ex)
+                print("Succsesfully added Sales CSV into PipelineMiniProject")
 
-def load_third_party(connection, file_path_csv ):
-    cursor = connection.cursor()
-    insert_stmt = ("INSERT INTO sales (ticket_id, trans_date, event_id, event_name, event_date, event_type, event_city, customer_id, price, num_tickets) " 
-                    "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    )
-    for record in file_path_csv:
-        cursor.execute(insert_stmt,record)
-    
-    connection.commit()
-    cursor.close()
-    return
+    def query_popular_tickets(self,connection):
+        sql_statement = 'SELECT * FROM sales ORDER BY num_tickets DESC LIMIT 3'
+        cursor = connection.cursor()
+        cursor.execute(sql_statement)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
 
-def query_popular_tickets(connection):
-    sql_statement = 'SELECT * FROM sales ORDER BY num_tickets DESC LIMIT 3'
-    cursor = connection.cursor()
-    cursor.execute(sql_statement)
-    records = cursor.fetchall()
-    cursor.close()
-    return records
+    def show_top3_records(self,records):
+        print("Here are the most popular tickets in the past month.")
+        print("-"+ records1)
+        print("-"+ records2)
+        print("-"+  records3)
 
+pipeline = Connect_toDB
+pipeline.load_third_party(self.connection,"/Users/kobihancz/Downloads/Springboard Data engineering/Data Pipeline Mini Project /third_party_sales_1.csv")
+piepline.query_popular_tickets(self.connection)
+pipeline.show_top3_records
 
 
 
 
-get_db_connection()
-# load_third_party(connection, '/Users/kobihancz/Downloads/Springboard Data engineering/Data Pipeline Mini Project /third_party_sales_1.csv')
-# query_popular_tickets(connection)
- 
+
 
  
 
